c6e0d3ba60a8695e8fa05458d06688fea73055b2
yoyesu <yoyes15@gmail.com>
2022-10-31 17:22:39 +0000

    logging

---
+++ src/main/java/com/sparta/booLeans/simulator/logging/CustomLoggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.sparta.booLeans.simulator.logging;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CustomLoggerConfiguration {
+
+    public static final Logger myLogger = Logger.getLogger("mylogger");
+    private static CustomLoggerConfiguration instance = null;
+
+    public static CustomLoggerConfiguration getInstance() {
+        if (instance == null) {
+            getCustomLoggerConfiguration(myLogger);
+            instance = new CustomLoggerConfiguration();
+        }
+        return instance;
+    }
+
+    public static void getCustomLoggerConfiguration(Logger logger) {
+        logger.setUseParentHandlers(false);
+        logger.addHandler(CustomFileHandler.getFileHandler());
+        logger.setLevel(Level.ALL);
+    }
+}
