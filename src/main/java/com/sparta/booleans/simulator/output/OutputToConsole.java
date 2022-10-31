ce3101669d70c4129fbe797d2c4b0d70e774d110
Will <will.slater01@outlook.com>
2022-10-31 17:36:44 +0000

    minor changes

--- src/main/java/com/sparta/booleans/simulator/output/OutputToConsole.java
+++ src/main/java/com/sparta/booleans/simulator/output/OutputToConsole.java
@@ -1,12 +1,23 @@
 package com.sparta.booleans.simulator.output;
 
-public class OutputToConsole {
+public class OutputToConsole implements OutputInterface{
 
-    public static void outputter(int years)
+    public static void outPutter(int years, int traineeCentre, int totalNumberofTrainees, int fullCentre, int traineesWaiting)
     {
+        System.out.println("+-----------------------------------------+");
         System.out.println("Number of years simulation ran for: " + years);
+        int months = years*12;
+        System.out.println("Number of months simulation ran for: " + months);
+        System.out.println("Number of traineeCentres created: " + traineeCentre + " of which " + fullCentre + " are full");
+        System.out.println("Number of trainees: " + totalNumberofTrainees);
+        System.out.println("Number of trainees waiting: " + traineesWaiting);
+        System.out.println("+-----------------------------------------+");
 
 
+    }
+
+    @Override
+    public void sendOutput() {
 
     }
 }
