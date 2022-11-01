0d2bd201323f49c535cbbf340b334240d50bb47b
Lucas M <lucas.m.marchi@outlook.com>
2022-11-01 17:03:53 +0000

    Implemented Starter class

--- src/main/java/com/sparta/booleans/controller/Starter.java
+++ src/main/java/com/sparta/booleans/controller/Starter.java
@@ -1,6 +1,12 @@
 package com.sparta.booleans.controller;
 
+import com.sparta.booleans.model.DTO;
 import com.sparta.booleans.utility.logging.CustomLoggerConfiguration;
+import com.sparta.booleans.view.CSVWriter;
+import com.sparta.booleans.view.input.TimeFrameInputter;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Starter {
 
@@ -10,6 +16,24 @@ public class Starter {
         // This customises the log , instance is not used.
         CustomLoggerConfiguration startLoggerConfig = CustomLoggerConfiguration.getInstance();
         //
+
+        Logger logger = CustomLoggerConfiguration.myLogger;
+
+        logger.log(Level.INFO, " The simulation has starter");
+
+        TimeFrameInputter imputter = new TimeFrameInputter();
+
+        DTO currentDTO = Simulator.runSimulation(imputter.getTotalMonths());
+
+        logger.log(Level.INFO, " The simulation has completed");
+
+
+        CSVWriter writer = new CSVWriter();
+
+        writer.sendOutput(currentDTO);
+        writer.writeToFile();
+
+        logger.log(Level.INFO, " The CSV Report is ready in the resources folder");
 
     }
 }
