63db24d80774ba096540d7413064cd48dc604fab
eco999 <justin02_steven@live.co.uk>
2022-11-01 14:58:52 +0000

    Added another exception for entering negative numbers

--- src/main/java/com/sparta/booleans/view/input/TimeFrameInputter.java
+++ src/main/java/com/sparta/booleans/view/input/TimeFrameInputter.java
@@ -1,6 +1,7 @@
 package com.sparta.booleans.view.input;
 
 import com.sparta.booleans.exceptions.InvalidTimeFrameMonthException;
+import com.sparta.booleans.exceptions.NegativeIntInputException;
 
 import java.util.Scanner;
 
@@ -11,23 +12,27 @@ public class TimeFrameInputter implements Inputable{
     @Override
     public int getInputInt() {
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        while (input < 0) {
-            System.out.println("Invalid input");
+        int input = -1;
+        try{
             input = scanner.nextInt();
+            if(input < 0){
+                throw new NegativeIntInputException();
+            }
+        }catch (NegativeIntInputException e){
+            System.out.println(e.getMessage());
+            System.out.println("Re-enter a number: ");
+            input = getInputInt();
         }
         return input;
     }
 
     @Override
-    public int getTotalMonths()
-    {
+    public int getTotalMonths() {
         System.out.println("Enter Years and months for simulation");
         return  (getYears()*12) + getMonths();
     }
 
-    public int getTotalMonths(int years, int months)
-    {
+    public int getTotalMonths(int years, int months) {
         System.out.println("Enter Years and months for simulation");
         return  (years * 12) + months;
     }
@@ -42,8 +47,7 @@ public class TimeFrameInputter implements Inputable{
         int input = 999;
         try{
             input = getInputInt();
-            if(input > 11)
-            {
+            if(input > 11) {
                 throw new InvalidTimeFrameMonthException();
             }
         }catch (InvalidTimeFrameMonthException e){
