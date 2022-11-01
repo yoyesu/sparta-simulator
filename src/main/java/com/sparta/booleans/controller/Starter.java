95bb11b0ebcb0abf44f3f2b80a4ffc5a7559cdff
Lucas M <lucas.m.marchi@outlook.com>
2022-11-01 15:53:57 +0000

    Initialised CustomLogger Config

--- src/main/java/com/sparta/booleans/controller/Starter.java
+++ src/main/java/com/sparta/booleans/controller/Starter.java
@@ -1,9 +1,15 @@
 package com.sparta.booleans.controller;
 
+import com.sparta.booleans.utility.logging.CustomLoggerConfiguration;
+
 public class Starter {
 
     public void start() {
 
+
+        // This customises the log , instance is not used.
+        CustomLoggerConfiguration startLoggerConfig = CustomLoggerConfiguration.getInstance();
+        //
 
     }
 }
