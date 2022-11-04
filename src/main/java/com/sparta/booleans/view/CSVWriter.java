18c0dcbb9949bb1b549da148ce28af0a324370f7
Lucas M <lucas.m.marchi@outlook.com>
2022-11-04 10:49:01 +0000

    CSVWriter now has logging and a getter() for the CSV_FILE_NAME field

--- src/main/java/com/sparta/booleans/view/CSVWriter.java
+++ src/main/java/com/sparta/booleans/view/CSVWriter.java
@@ -17,28 +17,38 @@ public class CSVWriter implements OutputInterface {
 
     public Logger logger = CustomLoggerConfiguration.myLogger;
 
-    private String CSV_FILE_NAME;
+    private final String CSV_FILE_NAME;
     private final List<List<String>> simulationOutputs = new ArrayList<>(new ArrayList<>());
 
     public CSVWriter() {
+        logger.log(Level.FINE, " CSWWriter has been instantiated");
+
         LocalDateTime time = LocalDateTime.now();
         this.CSV_FILE_NAME = "src/main/resources/simulation_report("
                 + time.format(DateTimeFormatter.ofPattern("YYYY-MM-dd_HH-mm-ss-SS"))
                 + ").csv";
+
+        logger.log(Level.FINE, " CSV File Name has been created with timestamp");
+    }
+
+    // Getter for testing...
+    public String getCSV_FILE_NAME() {
+        return this.CSV_FILE_NAME;
     }
 
     @Override
     public void sendOutput(MappedDTO simulationData) {
         if (simulationData == null) {
+            logger.log(Level.WARNING, " Invalid NULL param passed to CSVWriter's sendOutput()");
             throw new IllegalArgumentException("DTO must not be null");
         }
-        logger.log(Level.INFO, " Simulation Data has been accepted into ");
+        logger.log(Level.FINE, " CSVWriter instance has been called at sendOutput() with valid param");
         acceptSimulationData(simulationData);
     }
 
-    // Takes a list of DTOs, one for each contiguous run of the simulation
-    // Converts each DTO to a line (String)
+
     private void acceptSimulationData(MappedDTO simulationData) {
+
         List<String> data = new ArrayList<>();
 
         data.add("" + simulationData.getTotalMonths());
@@ -65,6 +75,8 @@ public class CSVWriter implements OutputInterface {
         data.add("" + simulationData.getUnhappyClients());
 
         simulationOutputs.add(data);
+
+        logger.log(Level.FINE, " Simulation Data has been accepted into the CSVWriter instance");
     }
 
     private String convertToCSV(List<String> data) {
@@ -76,13 +88,22 @@ public class CSVWriter implements OutputInterface {
      */
     public void writeToFile() {
 
+        logger.log(Level.FINE, " CSVWriter's writeToFile() has been called");
+
+
         File csvOutputFile = new File(CSV_FILE_NAME);
         try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
             pw.append(produceHeader());
             simulationOutputs.stream()
                     .map(this::convertToCSV)
                     .forEach(pw::println);
+
+            logger.log(Level.FINE, " CSV File has been written");
+
         } catch (FileNotFoundException e) {
+
+            logger.log(Level.WARNING, " CSVWriter's writeToFile() has thrown Exception");
+
             throw new RuntimeException(e);
         }
     }
@@ -92,8 +113,8 @@ public class CSVWriter implements OutputInterface {
         for (CourseType type : CourseType.values()) {
             header += type.name() + "_Trainees_Waiting,"
                     + type.name() + "_Trainees_Training,"
-                    + type.name() + "_Trainees_On_Bench"
-                    + type.name() + "_Trainees_With_Client";
+                    + type.name() + "_Trainees_On_Bench,"
+                    + type.name() + "_Trainees_With_Client,";
         }
         for (TrainingCentreType type : TrainingCentreType.values()) {
             header += type.name() + "S_Open,"
@@ -101,6 +122,9 @@ public class CSVWriter implements OutputInterface {
                     + type.name() + "S_Full,";
         }
         header += "Happy_Clients,Unhappy_Clients\n";
+
+        logger.log(Level.FINE, " CSVWriter's produceHeader() has run");
+
         return header;
     }
 
