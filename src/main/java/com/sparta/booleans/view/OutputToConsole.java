b4d2cf3f8caa697f544d80a7f75cf21acc158c6d
Lucas M <lucas.m.marchi@outlook.com>
2022-11-04 12:25:20 +0000

    Changed OutputToConsole() standard out format

--- src/main/java/com/sparta/booleans/view/OutputToConsole.java
+++ src/main/java/com/sparta/booleans/view/OutputToConsole.java
@@ -1,7 +1,6 @@
 package com.sparta.booleans.view;
 
 import com.sparta.booleans.model.CourseType;
-import com.sparta.booleans.model.DTO;
 import com.sparta.booleans.model.MappedDTO;
 import com.sparta.booleans.model.TrainingCentreType;
 
@@ -11,29 +10,32 @@ public class OutputToConsole implements OutputInterface{
     {
         System.out.println("+-----------------------------------------+");
         System.out.println("Number of months simulation ran for: " + simulationData.getTotalMonths());
-        System.out.println("\nTrainees: ");
+        System.out.println("\n\nTrainees: \n");
         for (CourseType type: CourseType.values()) {
-            System.out.println(type.name() + " Trainees Training: "
+            System.out.println(type.name() + " Trainees: ");
+            System.out.println(" -> Training: "
                     + simulationData.getTraineesTraining().get(type));
-            System.out.println(type.name() + " Trainees Waiting: "
+            System.out.println(" -> Waiting: "
                     + simulationData.getTraineesWaiting().get(type));
-            System.out.println(type.name() + " Trainees On Bench: "
+            System.out.println(" -> Without a Client: "
                     + simulationData.getTraineesOnBench().get(type));
-            System.out.println(type.name() + " Trainees With Clients: "
+            System.out.println(" -> With Clients: "
                     + simulationData.getTraineesWithClient().get(type));
         }
-        System.out.println("\nTraining Centres: ");
+        System.out.println("\n\nTraining Centres: \n");
         for (TrainingCentreType type: TrainingCentreType.values()) {
-            System.out.println(type.name() + "S Open: "
+            System.out.println(type.name() + ": ");
+            System.out.println(" -> Total Opened: "
                     + simulationData.getOpenCentres().get(type));
-            System.out.println(type.name() + "S Closed: "
-                    + simulationData.getClosedCentres().get(type));
-            System.out.println(type.name() + "S Full: "
+            System.out.println("    of which, ");
+            System.out.println("             -> Currently Full: "
                     + simulationData.getFullCentres().get(type));
+            System.out.println("             -> Currently Closed: "
+                    + simulationData.getClosedCentres().get(type));
         }
-        System.out.println("\nClients:");
-        System.out.println("Happy Clients: " + simulationData.getHappyClients());
-        System.out.println("Unhappy Clients: " + simulationData.getUnhappyClients());
+        System.out.println("\n\nClients: \n");
+        System.out.println(" -> Happy Clients: " + simulationData.getHappyClients());
+        System.out.println(" -> Unhappy Clients: " + simulationData.getUnhappyClients());
         System.out.println("+-----------------------------------------+");
     }
 
